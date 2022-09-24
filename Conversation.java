9cc64b36bc46767868b372bd1f0da867f6dfe714
JOANNA LI <jli40@smith.edu>
2022-09-24 14:40:01 -0400

    Alex Lee scanner tutorial

--- Conversation.java
+++ Conversation.java
@@ -5,9 +5,19 @@ class Conversation {
   public static void main(String[] args) {
     // You will start the conversation here.
     Scanner scan = new Scanner(System.in); 
+    
     System.out.println("What's your first name?"); 
     String name = scan.next();
-    System.out.println(name);  
+
+    System.out.println("What is your age?"); 
+    int age = scan.nextInt();
+
+    System.out.println("What's your senior quote?"); 
+    String quote = scan.next();
+    quote += scan.nextLine(); 
+
+    System.out.println("Thank you " + name + ", you are " + age + " years old");
+    System.out.println("and your senior quote is " + quote); 
   }
 
 } 
