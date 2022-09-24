9e1337c46be7210384fafae45aa697de862c0088
JOANNA LI <jli40@smith.edu>
2022-09-24 14:13:20 -0400

    Alex Lee scanner tutorial

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 class Conversation {
 
-  public static void main(String[] arguments) {
+  public static void main(String[] args) {
     // You will start the conversation here.
+    Scanner scan = new Scanner(System.in); 
+    System.out.println("What's your first name?"); 
+    String name = scan.next();
+    System.out.println(name);  
   }
-}
+
+} 
